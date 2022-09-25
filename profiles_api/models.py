12232c1a2e15512d0470926718380e271c7f694a
Abdullah <garra0702@gmail.com>
2022-09-25 13:32:12 +0300

    models and db

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,9 +1,39 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
-from django.contrib.auth.models import PermessionsMixin
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
+class UserProfileManager(BaseUserManager):
+    '''manager for user profile'''
 
 
-class UserProfile(AbstractBaseUser,PermessionsMixin):
+
+    def create_user(self, email, name, password=None):
+        '''creating a new user profile '''
+        if not email:
+            raise ValueError('Users must have an email')
+
+        email = self.normalize_email(email)
+        user = self.model(email = email, name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        '''create new super user'''
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using = self._db)
+
+        return user
+
+
+
+class UserProfile(AbstractBaseUser,PermissionsMixin):
     """Database model for users in the system"""
     email = models.EmailField(max_length=255,unique = True)
     name = models.CharField(max_length=255)
@@ -14,6 +44,7 @@ class UserProfile(AbstractBaseUser,PermessionsMixin):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
+
     def get_full_name(self):
         return self.name
     def get_short_name(self):
